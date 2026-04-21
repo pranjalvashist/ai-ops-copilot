55644941da670eafefd922f59390f5ca454e98ca
pranjalvashist <vashist.pranjal21@gmail.com>
2026-04-21 18:57:13 -0400

    fix cors for deployment

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],  # allow all for now
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
